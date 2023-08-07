831b9824033f10728bf6bdc074d3eb59bc544453
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-08-07 20:28:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use environment variable or default to port 3000
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Define a basic route
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
