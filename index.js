0e301495b6e2b73a3b13bdc023e45d250036e3bc
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-08-09 19:06:45 +0530

    connected mongo container with moongoose

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = process.env.PORT || 3000; // Use environment variable or default to port 3000
+
+mongoose
+  .connect("mongodb://root:example@mongo:27017/?authSource=admin")
+  .then(() => console.log("Succesfully connected"))
+  .catch((e) => console.log(e));
 
 // Serve static files from the "public" folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Define a basic route
-app.get('/', (req, res) => {
-    res.send('Hello, Express ');
+app.get("/", (req, res) => {
+  res.send("Hello, Express ");
 });
 
+const port = process.env.PORT || 3000; // Use environment variable or default to port 3000
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
